2136a27b4d68ec8b67137263c592e6a5d5ebdc36
Coral Engel <81955555+coralen@users.noreply.github.com>
2023-04-11 18:26:53 +0300

    Update question.java

--- semesterProject/src/semesterProject/question.java
+++ semesterProject/src/semesterProject/question.java
@@ -1,17 +1,64 @@
 package semesterProject;
+
 import semesterProject.answer;
 
 public class question {
 
 	String question;
-	answer[] answers;
-	
-	public void setQuestion(String questionInput) {
-		this.question = questionInput;
+	answer[] answers = new answer[10];
+
+	public question(String question) {
+		this.question = question;
 	}
-	
-	public void setAnswers(answer answerInput) {
-		
+
+	public question(answer[] answers) {
+		this.answers = answers;
 	}
-	
+
+	public question(String question, answer[] answers) {
+		this.question = question;
+		this.answers = answers;
+	}
+
+	public String getQuestion() {
+		return this.question;
+	}
+
+	public answer[] getAnswers() {
+		return this.answers;
+	}
+
+	public void printQuestion() {
+		System.out.println(question);
+	}
+
+	public void printAnswers() {
+		for (answer ans : this.answers) {
+			ans.printAnswer();
+			System.out.print(" - ");
+			ans.printIsRightAnswer();
+		}
+	}
+
+	public void addAnswer(answer answerInput) {
+		boolean noRoomLeft = true;
+		for (int i = 0; i < this.answers.length; i++) {
+			if (this.answers[i] == null) {
+				this.answers[i] = answerInput;
+				noRoomLeft = false;
+			}
+		}
+		if (noRoomLeft) {
+			System.out.println("There are already 10 answers. Limit has been reached.");
+		}
+	}
+
+	public void removeAnswer(answer answerInput) {
+		for (int i = 0; i < this.answers.length; i++) {
+			if (this.answers[i] == answerInput) {
+				this.answers[i] = null;
+			}
+		}
+	}
+
 }
