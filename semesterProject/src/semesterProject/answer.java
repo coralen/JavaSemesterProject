89d227eb6e20613713e2833a48299b462f2a877c
Coral Engel <81955555+coralen@users.noreply.github.com>
2023-04-11 18:26:21 +0300

    Update answer.java

--- semesterProject/src/semesterProject/answer.java
+++ semesterProject/src/semesterProject/answer.java
@@ -1,24 +1,41 @@
 package semesterProject;
 
 public class answer {
-	
-	private String answer;
-	private boolean isRightAnswer;
-	
-	public void setAnswer(String answerInput) {
-		this.answer = answerInput;
+
+	String answer;
+	boolean isRightAnswer;
+
+	public answer(String answer) {
+		this.answer = answer;
 	}
-	
-	public void setIsRightAnswer(boolean isRightAnswerInput) {
-		this.isRightAnswer = isRightAnswerInput;
+
+	public answer(boolean isRightAnswer) {
+		this.isRightAnswer = isRightAnswer;
 	}
-	
+
+	public answer(String answer, boolean isRightAnswer) {
+		this.answer = answer;
+		this.isRightAnswer = isRightAnswer;
+	}
+
 	public String getAnswer() {
 		return answer;
 	}
-	
+
 	public boolean getIsRightAnswer() {
 		return isRightAnswer;
 	}
-	
+
+	public void printAnswer() {
+		System.out.println(this.answer);
+	}
+
+	public void printIsRightAnswer() {
+		if (this.isRightAnswer) {
+			System.out.print("This is the right answer");
+		} else {
+			System.out.print("This is not the right answer");
+		}
+	}
+
 }
