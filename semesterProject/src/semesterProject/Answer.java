712ff2660a8e813546d93d6a7e7d150038f23c88
Coral Engel <81955555+coralen@users.noreply.github.com>
2023-05-07 14:32:32 +0300

    Update Answer.java

--- semesterProject/src/semesterProject/Answer.java
+++ semesterProject/src/semesterProject/Answer.java
@@ -1,10 +1,10 @@
-package semesterProject;
+package BarGvili_206419152_CoralEngel_208455659;
 
 public class Answer {
 
 	private String answer;
 	private boolean isRightAnswer;
-	
+
 	public Answer(String answer) {
 		this.answer = answer;
 	}
@@ -25,7 +25,7 @@ public class Answer {
 	public boolean getIsRightAnswer() {
 		return this.isRightAnswer;
 	}
-	
+
 	public void setIsRightAnswer(boolean isRightAnswer) {
 		this.isRightAnswer = isRightAnswer;
 	}
