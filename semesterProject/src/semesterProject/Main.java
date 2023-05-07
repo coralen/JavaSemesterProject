3d140a3c2782353ee3287ae387cbdfd136dd048b
Coral Engel <81955555+coralen@users.noreply.github.com>
2023-05-07 14:32:11 +0300

    Update Main.java

--- semesterProject/src/semesterProject/Main.java
+++ semesterProject/src/semesterProject/Main.java
@@ -1,15 +1,17 @@
-package semesterProject;
+package BarGvili_206419152_CoralEngel_208455659;
 
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 
-		int userChoice, numOfQuestionsSelected, qstIdx, ansIdx, qstNumber, ansNumber, ansCounterForFinal,
-				numOfQuestions, numOfAnswers, finalQstIdx = 0, isRightCount = 0;
+		int userChoice, numOfQuestionsSelected, numOfAnswersSelected, qstIdx, ansIdx, qstNumber, ansNumber,
+				numOfQuestionsAllowed, numOfAnswersAllowed, finalQstIdx = 0, isRightCount = 0;
 		String newAns, qstString, ansString;
 		boolean ansType, newAnsType;
 		Question[] questions, finalQuestions;
@@ -28,6 +30,7 @@ public class Main {
 					+ "4 - Delete an answer\n"
 					+ "5 - Delete a question with its answers\n" 
 					+ "6 - Create a test\n" + "0 - Exit the menu\n");
+			
 			userChoice = scanSelection.nextInt();
 
 			switch (userChoice) {
@@ -95,59 +98,60 @@ public class Main {
 			case 6:
 				finalQstIdx = 0;
 				questions = myTest.getQuestions();
-				numOfQuestions = myTest.getNumberOfQuestions();
-				printerExam = ExamRepo.getExamPrinter();
-				printerSolution = ExamRepo.getSolutionPrinter();
+				numOfQuestionsAllowed = myTest.getNumberOfQuestions();
+				printerExam = getExamPrinter();
+				printerSolution = getSolutionPrinter();
 
 				System.out.println("Please enter how many questions you want in the exam");
 				numOfQuestionsSelected = scanAnswer.nextInt();
-				
-				while (numOfQuestionsSelected > numOfQuestions) {
+
+				while (numOfQuestionsSelected > numOfQuestionsAllowed) {
 					System.out.println("You chose too many questions. Please enter a different amount");
 					numOfQuestionsSelected = scanAnswer.nextInt();
 				}
 				finalTest = new ExamRepo(numOfQuestionsSelected);
-				
+
 				for (int i = 0; i < numOfQuestionsSelected; i++) {
 					System.out.println("Enter the index of the question you want to add to the exam");
 					scanAnswer.nextLine();
 					qstIdx = scanAnswer.nextInt();
-					scanAnswer.nextLine();
-					
-					while (qstIdx > questions.length || qstIdx < 1) {
-						System.out.println("This question don't exist. Please enter a different index of question");
+
+					while (qstIdx > numOfQuestionsAllowed || qstIdx < 1) {
+						System.out.println("This question doesn't exist. Please enter a different index of question");
 						qstIdx = scanAnswer.nextInt();
-						scanAnswer.nextLine();
 					}
-					
+					numOfAnswersAllowed = questions[qstIdx - 1].getNumberOfAnswers();
 					finalTest.addQuestion(questions[qstIdx - 1].getQuestion());
 					printerExam.print("  " + (i + 1) + ") " + questions[qstIdx - 1].getQuestion() + "\n");
 					printerSolution.print("  " + (i + 1) + ") " + questions[qstIdx - 1].getQuestion() + "\n");
-					
-					numOfAnswers = questions[qstIdx - 1].getNumberOfAnswers();
+
+					numOfAnswersAllowed = questions[qstIdx - 1].getNumberOfAnswers();
 					answers = questions[qstIdx - 1].getAnswers();
 					System.out.println("Enter the indexes of the answers to this question."
 							+ "Press any key other than a number to stop");
 
-					ansCounterForFinal = 0;
+					numOfAnswersSelected = 0;
 					isRightCount = 0;
 					while (scanAnswer.hasNextInt()) {
 						ansIdx = scanAnswer.nextInt();
-						
-						scanAnswer.nextLine();
-						ansCounterForFinal++;
+						while (ansIdx > numOfAnswersAllowed || ansIdx < 1) {
+							System.out.println("Invalid index for an answer");
+							ansIdx = scanAnswer.nextInt();
+						}
+						numOfAnswersSelected++;
 						ans = answers[ansIdx - 1];
 						if (ans.getIsRightAnswer()) {
 							isRightCount++;
 						}
 						finalTest.addAnswer(i, ans.getAnswer(), ans.getIsRightAnswer());
+						System.out.println("Enter another index");
 					}
 
 					finalQuestions = finalTest.getQuestions();
 					finalAnswers = finalQuestions[finalQstIdx].getAnswers();
 
 					int j;
-					for (j = 0; j < ansCounterForFinal; j++) {
+					for (j = 0; j < numOfAnswersSelected; j++) {
 						if (isRightCount > 1 || isRightCount == 0) {
 							finalAnswers[j].setIsRightAnswer(false);
 						}
@@ -158,22 +162,22 @@ public class Main {
 								+ finalAnswers[j].getIsRightAnswer() + "\n");
 					}
 
-					printerExam.print("    " + (j + 1) + ") More than one answer is correct\n" 
-					+ "    " + (j + 2) + ") No answer is correct\n");
+					printerExam.print("    " + (j + 1) + ") More than one answer is correct\n" + "    " + (j + 2)
+							+ ") No answer is correct\n");
 					if (isRightCount > 1) {
 
-						printerSolution.print("    " + (j + 1) + ") More than one answer is correct - true\n" 
-						+ "    " + (j + 2) + ") No answer is correct - false\n");
+						printerSolution.print("    " + (j + 1) + ") More than one answer is correct - true\n" + "    "
+								+ (j + 2) + ") No answer is correct - false\n");
 					} else if (isRightCount == 0) {
-						printerSolution.print("    " + (j + 1) + ") More than one answer is correct - false\n" 
-					+ "    " + (j + 2) + ") No answer is correct - true\n");
+						printerSolution.print("    " + (j + 1) + ") More than one answer is correct - false\n" + "    "
+								+ (j + 2) + ") No answer is correct - true\n");
 					} else {
-						printerSolution.print("    " + (j + 1) + ") More than one answer is correct - false\n" 
-								+ "    " + (j + 2) + ") No answer is correct - false\n");
+						printerSolution.print("    " + (j + 1) + ") More than one answer is correct - false\n" + "    "
+								+ (j + 2) + ") No answer is correct - false\n");
 					}
-							
-					finalQstIdx++;
 
+					finalQstIdx++;
+					scanAnswer.nextLine();
 				}
 				scanAnswer.nextLine();
 				printerExam.close();
@@ -183,6 +187,29 @@ public class Main {
 			}
 
 		} while (userChoice != 0);
+	}
+
+	public static String getTestDateTime() {
+		LocalDateTime myDateTime = LocalDateTime.now();
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm");
+		String formattedDate = myDateTime.format(myFormatObj);
+		return formattedDate;
+	}
+
+	public static PrintWriter getExamPrinter() throws FileNotFoundException {
+
+		String formattedDate = getTestDateTime();
+		File exam = new File("exam_" + formattedDate);
+		PrintWriter examPrinter = new PrintWriter(exam);
+		return examPrinter;
+	}
+
+	public static PrintWriter getSolutionPrinter() throws FileNotFoundException {
+
+		String formattedDate = getTestDateTime();
+		File solution = new File("solution_" + formattedDate);
+		PrintWriter solutionPrinter = new PrintWriter(solution);
+		return solutionPrinter;
 	}
 
 }
